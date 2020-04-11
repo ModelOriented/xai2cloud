0f8c56426e64dfe13fa73ab705328e6e9b95ea51
Adamoso <a.rydelek@student.mini.pw.edu.pl>
2020-04-11 14:27:17 +0200

    Updating tests from file

--- tests/testthat/test_get_template_data.R
+++ tests/testthat/test_get_template_data.R
@@ -3,25 +3,44 @@ context("Check get_template_data() function")
 source("helper_file.R")
 
 test_that("Type of output",{
+  # From explainer object
   template_data <- get_template_data(exp_name = explainer_titanic)
   expect_is(template_data, "list")
   expect_is(unlist(template_data), "character")
+  # From .rda file
+  template_data_file <- get_template_data(exp_name = "./../objects_for_tests/explain_titanic.rda")
+  expect_is(template_data_file, "list")
+  expect_is(unlist(template_data_file), "character")
 })
 
 test_that("All parameters are included",{
+  # From explainer object
   template_data <- get_template_data(exp_name = explainer_titanic)
   params_len <- sapply(strsplit(template_data$params, ","), length)
   params_amp_len <- sapply(strsplit(template_data$params_amp, ","), length)
   params_x_len <- sapply(strsplit(template_data$params_x, ","), length)
   expect_equal(params_len, params_amp_len)
   expect_equal(params_len, params_x_len)
+  # From .rda file
+  template_data_file <- get_template_data(exp_name = "./../objects_for_tests/explain_titanic.rda")
+  params_len_file <- sapply(strsplit(template_data_file$params, ","), length)
+  params_amp_len_file <- sapply(strsplit(template_data_file$params_amp, ","), length)
+  params_x_len_file <- sapply(strsplit(template_data_file$params_x, ","), length)
+  expect_equal(params_len_file, params_amp_len_file)
+  expect_equal(params_len_file, params_x_len_file)
 })
 
 test_that("Parameters were divided into numerical and factors correctly",{
+  # From explainer object
   template_data <- get_template_data(exp_name = explainer_titanic)
   all_params <- sapply(strsplit(template_data$params, ","), length)
   factors_and_numeric <- sapply(strsplit(template_data$params_factor, ","), length) + sapply(strsplit(template_data$params_numeric, ","), length)
   expect_equal(factors_and_numeric, all_params)
+  # From .rda file
+  template_data_file <- get_template_data(exp_name = explainer_titanic)
+  all_params_file <- sapply(strsplit(template_data_file$params, ","), length)
+  factors_and_numeric_file <- sapply(strsplit(template_data_file$params_factor, ","), length) + sapply(strsplit(template_data_file$params_numeric, ","), length)
+  expect_equal(factors_and_numeric_file, all_params_file)
 })
 
 file.remove("explainer_titanic.rda")
