43de717a68914d173f73d5f1cc27faffb54bec05
Adamoso <a.rydelek@student.mini.pw.edu.pl>
2020-04-11 16:34:18 +0200

    Adding examples and vignette update

--- R/deploy_explainer.R
+++ R/deploy_explainer.R
@@ -1,10 +1,11 @@
 #' Deploy An Explainer
 #'
 #' This function deploys an explainer as an REST API with an corresponding swagger.
-#' Deployment can be done either locally or directly to the DigitalOcean's droplet.
 #' A new folder will be created in your working directory containing a plumber R file and an .rda file of the explainer.
+#' Deployment can be done either locally or directly to the DigitalOcean's droplet.
+#' Full guide on setting up DigitalOcean's droplet and deploying to the cloud can be found on package's GitHub page.
 #'
-#' @param exp_name DALEX explainer object or an .rda filename containing the explainer
+#' @param exp_name DALEX explainer object or an .rda filename in your working directory containing the explainer
 #' @param droplet If you want to deploy the API locally leave the value as \code{NA}. If you want to deploy to DigitalOcean, use the droplet's ID which can be checked by using \code{analogsea::droplets()}
 #' @param port Port on which you want your API to be deployed
 #' @param deploy Boolean telling whether the plumber file is run on set port
@@ -14,52 +15,60 @@
 #' @import DALEX
 #' @importFrom whisker whisker.render
 #' @examples
-#' \dontrun{
 #' # Using an explainer object, locally
-#' # Load data
-#' titanic <- na.omit(DALEX::titanic)
-#' model_titanic_rf <- randomForest::randomForest(survived == "yes" ~ gender + age + class + embarked +
-#' fare + sibsp + parch,  data = titanic)
-#' explain_titanic_rf <- DALEX::explain(model_titanic_rf,
-#' data = titanic[,-9],
-#' y = titanic$survived == "yes",
-#' label = "Random Forest v7",
-#' colorize = FALSE)
+#' # Load data and create model
+#' model_data <- DALEX::titanic_imputed
+#' titanic_glm <- glm(survived ~ gender + age + fare,
+#'                        data = model_data, family = "binomial")
+#' # Create DALEX explainer
+#' explain_titanic_glm <- explain(titanic_glm,
+#'                            data = model_data,
+#'                            y = model_data$survived,
+#'                            label = "glm")
+#' # Deploy the API
+#' # If you want the API to deploy automatically, set deploy parameter to TRUE
 #'
-#' deploy_explainer(explain_titanic_rf, title = "Titanic", port = 8070)
+#' # Locally
+#' deploy_explainer(explain_titanic_glm,
+#'   title = "Titanic", port = 8070, deploy=FALSE)
 #'
-#' # Using an explainer object, to the cloud
-#' deploy_explainer(explain_titanic_rf, title = "Titanic", droplet = 185232162, port = 8070)
-#'
-#' # Using an .rda explainer file, locally
-#' deploy_explainer("explain_titanic_rf.rda", title = "Titanic", port = 8070)
-#'
-#' # Using an .rda explainer file, to the cloud
-#' deploy_explainer("explain_titanic_rf.rda", title = "Titanic", droplet = 185232162, port = 8080)
-#' }
-deploy_explainer <- function(exp_name, droplet = NA, port = 8088, deploy = TRUE, title = "xai2cloud"){
+#' # To the cloud
+#'\dontrun{
+#' analogsea::droplets()
+#' deploy_explainer(explain_titanic_glm,
+#'   title = "Titanic", droplet = 136232162,
+#'   port = 8080)
+#'}
+deploy_explainer <- function(exp_name, droplet = NA,
+                             port = 8088, deploy = TRUE, title = "xai2cloud"){
 
+  # Get data to fill the template
   template_data <- get_template_data(exp_name, title)
+  # Get whisker template
   template <- get_template()
-
+  # Rendering the template with provided data
   text_to_file <- whisker.render(template, template_data)
-
-  explain_name <- template_data$explain_name
+  # Creating a new directory with the explainer's name
+  explain_path <- template_data$explain_name
+  explain_name <- gsub(".*/","",template_data$explain_name)
   dir_name <- substr(explain_name, 1, nchar(explain_name)-4)
   dir.create(dir_name)
   old_wd <- getwd()
   new_wd <- paste(old_wd,"/",dir_name, sep="")
 
-  from_copy <- paste(old_wd,"/",explain_name, sep="")
+  # Copying the .rda file to the new directory
+  from_copy <- explain_path
   to_copy <- paste(new_wd,"/",explain_name, sep="")
   file.copy(from_copy, to_copy)
 
   setwd(new_wd)
+  # Creating the R file to be plumbed
   file_name_ext <- "plumber.R"
   file.create(file_name_ext)
   fileConn<-file(file_name_ext)
   writeLines(text_to_file, fileConn)
   close(fileConn)
+  # Deploying the API locally
   if(is.na(droplet)){
     pmodel <- plumb("plumber.R")
     setwd(old_wd)
@@ -69,7 +78,7 @@ deploy_explainer <- function(exp_name, droplet = NA, port = 8088, deploy = TRUE,
   }
   setwd(old_wd)
   local_path <- paste0(getwd(),"/",dir_name)
-  print(local_path)
+  # Deploying the API to DigitalOcean
   if(!is.na(droplet)){
     if(deploy==TRUE){
       plumber::do_deploy_api(droplet = droplet, path = dir_name, localPath = local_path, swagger = TRUE, port = port)
--- tests/testthat/test_deploy_explainer.R
+++ tests/testthat/test_deploy_explainer.R
@@ -3,12 +3,20 @@ context("Check deploy_explainer() function")
 source("helper_file.R")
 
 test_that("All files are created",{
+  # From object
   deploy_explainer(exp_name = explainer_titanic, droplet = NA, deploy=FALSE)
   expect_true("exp_name" %in% list.files())
   expect_true("exp_name.rda" %in% list.files())
   expect_true("plumber.R" %in% list.files("exp_name"))
   expect_true("exp_name.rda" %in% list.files("exp_name"))
+  # From file
+  deploy_explainer(exp_name = "./../objects_for_tests/explain_titanic.rda", droplet = NA, deploy=FALSE)
+  expect_true("explain_titanic" %in% list.files())
+  expect_true("plumber.R" %in% list.files("explain_titanic"))
+  expect_true("explain_titanic.rda" %in% list.files("explain_titanic"))
 })
+
 
 file.remove("exp_name.rda")
 unlink("exp_name", recursive=TRUE)
+unlink("explain_titanic", recursive=TRUE)
